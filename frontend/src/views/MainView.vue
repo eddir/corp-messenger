f293a335e62611a6698377f164cfeb9174cab489
Mikhail Kuznetsov <mixail.kuznetsov2001@yandex.ru>
2022-10-21 01:32:12 +0400

    front: initial commit

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>Hello, world!</div>
+</template>
+
+<script>
+    export default {
+        name: "MainView",
+        
+        mounted() {
+            this.init()
+        },
+
+        methods: {
+            init() {
+                this.$api.echo.test({ data: 'hello men!!!' }).then((res) => {
+                    console.log('test rest:', res)
+                }).catch((e) => {
+                    console.error(e)
+                })
+            }
+        }
+    }
+</script>
