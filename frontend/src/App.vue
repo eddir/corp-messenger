1d166e6cfaaa886729ff630e9a2ca0ee3c33ae11
Mikhail Kuznetsov <mixail.kuznetsov2001@yandex.ru>
2022-11-05 01:10:16 +0400

    add: лайауты для системы

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,22 @@
 <template>
-    <router-view />
+    <component :is="layout">
+        <router-view />
+    </component>
 </template>
 
-<style lang="less"></style>
+<script>
+import AuthLayout from "@/layouts/AuthLayout.vue"
+import MainLayout from "@/layouts/MainLayout.vue"
+
+export default {
+    computed: {
+        layout() {
+            return `${this.$route.meta.layout || 'auth'}-layout`
+        }
+    },
+
+    components: {
+        AuthLayout, MainLayout
+    }
+}
+</script>
