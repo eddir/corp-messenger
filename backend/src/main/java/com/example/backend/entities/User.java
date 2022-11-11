e406dfa6b0d91dcef2ce0e60c0184a9d20241a82
Anton Protsenko <protseng@mail.ru>
2022-11-11 11:48:41 +0400

    Добавил шифрование паролей

--- backend/src/main/java/com/example/backend/entities/User.java
+++ backend/src/main/java/com/example/backend/entities/User.java
@@ -16,5 +16,27 @@ public class User extends BaseEntity
     @Column(name = "app_role")
     protected ApplicationRole applicationRole;
 
+    public String getLogin() {
+        return login;
+    }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ApplicationRole getApplicationRole() {
+        return applicationRole;
+    }
+
+    public void setApplicationRole(ApplicationRole applicationRole) {
+        this.applicationRole = applicationRole;
+    }
 }
--- backend/src/main/java/com/example/backend/services/UserService.java
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -3,21 +3,26 @@ package com.example.backend.services;
 import com.example.backend.entities.User;
 import com.example.backend.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService
 {
-    UserRepo userRepo;
+    protected UserRepo userRepo;
+    @Autowired
+    protected BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepo userRepo)
+    public UserService(UserRepo userRepo, BCryptPasswordEncoder bCryptPasswordEncoder)
     {
         this.userRepo = userRepo;
+        this.passwordEncoder = bCryptPasswordEncoder;
     }
 
     public User register(User user)
     {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
