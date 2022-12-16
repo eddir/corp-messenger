222551fcc0125b9cff646041bad015ca63ab24f3
Anton Protsenko <protseng@mail.ru>
2022-12-17 02:17:32 +0400

    fix: Создание чатов пользователем

--- backend/src/main/java/com/example/backend/repositories/MessageRepository.java
+++ backend/src/main/java/com/example/backend/repositories/MessageRepository.java
@@ -1,10 +1,16 @@
 package com.example.backend.repositories;
 
+import com.example.backend.entities.Chat;
 import com.example.backend.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long>
 {
+    //@Query("select m from Message m where m.chat = :chat order by m.id")
+    //public List<Message> getMessageByChatByCountAntIntervalDefaultOrd(Chat chat, Long count,Message startMessage);
 }
